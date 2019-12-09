2936897bc92cc150d6d1f4174d66e9fd174275ca
sychevskiy <frilans.er@mail.ru>
2019-12-09 16:00:39 +1000

    Added visibility for phone

--- template/js/script.js
+++ template/js/script.js
@@ -2,19 +2,22 @@ const car = (name, model, year, owner, phone, image) => ({name, model, year, own
 
 const cars = [
     car("Ford", "Focus", "Max", 2016, "89231325421", "static/img/ford-focus.png"),
-    car("Ford", "MONDEO", "Max", 2016, "89231325421", "static/img/ford-MONDEO.png"),
-    car("Ford", "FIESTA", "Max", 2016, "89231325421", "static/img/ford-FIESTA.png")
+    car("Ford", "MONDEO", "Andree", 2014, "89462513521", "static/img/ford-MONDEO.png"),
+    car("Ford", "FIESTA", "Anna", 2012, "89324125213", "static/img/ford-FIESTA.png")
 ];
 
 new Vue({
     el: "#app",
     data: {
         cars: cars,
-        carActive: cars[0]
+        carActive: cars[0],
+        selectActive: 0,
+        phoneVisibility: false
     },
     methods: {
         selectCar: function(index) {
-            this.carActive = cars[index];
+            this.carActive = cars[index]
+            this.selectActive = index
         }
     }
 });
